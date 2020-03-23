9131b23a7da151c27a3d9650938f69e5ba4077dc
KoldunPodkradun <koldunpodkradun@gmail.com>
2020-03-23 18:39:32 +0300

    23.03.2020 18.40

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
+$('#popupOne').modal('show');
+
 $('div[data-target="#calendar"]').on('shown.bs.tab', function (event) {
     $('.items-time').removeClass('hidden');
 });
 
 $('div[data-target="#clients-list"]').on('shown.bs.tab', function (event) {
     $('.items-time').addClass('hidden');
+});
+
+$('.popup-purple-btn').on('click', function (event) {
+    $('#tireFitting').removeClass('opacity');
 });
 
 $('.calendar').slick();
